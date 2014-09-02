bf55611dd32b290e4de234eb8446d5ba00394c08
linaohan <linaohan1993@126.com>
2014-09-02 16:47:07 -0400

    stash?

--- protoauthor/models.py
+++ protoauthor/models.py
@@ -1,26 +1,9 @@
 from django.db import models
 import json
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- 
-
 
 # Create your models here.
 class Interface(models.Model):
     name = models.CharField(max_length=200)
-
-class User(models.Model):
-	
-	interface_id = models.CharField(max_length=200)
-	task = models.CharField(max_length=200)
-	timespent = models.CharField(max_length=200)
-	os = models.CharField(max_length=200)
-	browser = models.CharField(max_length=200)
-	age = models.CharField(max_length=200)
-	gender = models.CharField(max_length=200)
-	country = models.CharField(max_length=200)
-	evaluation = models.CharField(max_length=200)
-	experience = models.CharField(max_length=200)
-    
 
 class Widget(models.Model):
     interface = models.ForeignKey(Interface)
@@ -29,12 +12,9 @@ class Widget(models.Model):
     left = models.CharField(max_length=200)
     width = models.CharField(max_length=200)
     height = models.CharField(max_length=200)
-    active = models.BooleanField(default=True)
+	
 
     def getJSON(self):
-        return json.dumps(self.getDict())
-
-    def getDict(self):
         vals = {}
         vals['pk'] = self.pk
         vals['value'] = self.value
@@ -42,34 +22,7 @@ class Widget(models.Model):
         vals['left'] = self.left
         vals['width'] = self.width
         vals['height'] = self.height
-        vals['active'] = self.active
-        return vals
+        return json.dumps(vals)
 
-class Log(models.Model):
-    interface = models.ForeignKey(Interface)
-    widget = models.ForeignKey(Widget)
-    action = models.CharField(max_length=200)
-    value = models.CharField(max_length=200)
-    top = models.CharField(max_length=200)
-    left = models.CharField(max_length=200)
-    width = models.CharField(max_length=200)
-    height = models.CharField(max_length=200)
-    created = models.DateTimeField(auto_now_add=True)
 
-class Undo(models.Model):
-    interface = models.ForeignKey(Interface)
-    widget = models.ForeignKey(Widget)
-    action = models.CharField(max_length=200)
-    is_redo = models.BooleanField(default=False)
-    prev_value = models.CharField(max_length=200)
-    prev_top = models.CharField(max_length=200)
-    prev_left = models.CharField(max_length=200)
-    prev_width = models.CharField(max_length=200)
-    prev_height = models.CharField(max_length=200)
-    next_value = models.CharField(max_length=200)
-    next_top = models.CharField(max_length=200)
-    next_left = models.CharField(max_length=200)
-    next_width = models.CharField(max_length=200)
-    next_height = models.CharField(max_length=200)
-    created = models.DateTimeField(auto_now_add=True)
 
--- protofactory/wsgi.py
+++ protofactory/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for clutter_system project.
+WSGI config for protofactory project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,29 +8,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-import sys
-#import site
-
-#Add virtual environment
-#site.addsitedir('/var/venv/main_site/lib/python2.7/site-packages')
-
-# Configure PYTHONPATH
-paths = ["/home/cmaclell/protofactory/protofactory/",
-         "/home/cmaclell/protofactorym/protoauthor/",
-         "/home/cmaclell/protofactory/"]
-       #"/var/venv/main_site/lib/python2.7/site-packages"]
-
-for path in paths:
-    if path not in sys.path:
-            sys.path.append(path)
-
-#print(sys.path)
-
-# To fix multiple django instances on same domain.
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "clutter.server-settings")
-os.environ["DJANGO_SETTINGS_MODULE"] = "protofactory.server-settings"
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "protofactory.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
